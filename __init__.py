d9f4ccdb784a3f883a692839a8160b8c183ab8a0
Rounak Bhowmik <72032294+tank-king@users.noreply.github.com>
2023-07-12 21:23:06 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .gui import GUIManager
 
 
-def show_gui_popup(filename, size=None, fps=60):
-    GUIManager.run_until_close(filename, size, fps)
+def show_gui_popup(filename, size=None, offset=None, fps=60):
+    GUIManager.run_until_close(filename, size, offset, fps)
